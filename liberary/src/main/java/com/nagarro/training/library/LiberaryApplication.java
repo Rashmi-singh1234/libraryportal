c78b2bf8888ff776b26be8e2bdca6cdeab292c7e
Rashmi-singh1234 <rashmia2018cse@gmail.com>
2023-08-29 13:45:07 +0530

    initial commit

---
+++ liberary/src/main/java/com/nagarro/training/library/LiberaryApplication.java
@@ -0,0 +1,17 @@
+package com.nagarro.training.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class LiberaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberaryApplication.class, args);
+		String ad="fgn";
+		System.out.println(ad);
+
+	}
+
+}
---
+++ liberary/src/test/java/com/nagarro/liberary/LiberaryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nagarro.liberary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nagarro.training.library.LiberaryApplication;
+
+
+@SpringBootTest(classes=LiberaryApplication.class)
+class LiberaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
